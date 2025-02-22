6983813904c3bc9d442fac20cdc8a5d14903080c
CrystalSplitter <gamewhizzit@gmail.com>
2025-02-21 20:45:16 -0800

    Add Nix packaging files
    Eventually, these will hopefully be moved upstream.

---
+++ nix/package-core.nix
@@ -0,0 +1,43 @@
+{ shell-type }:
+{
+  pkgs,
+  python3,
+  stdenv,
+  ...
+}:
+
+stdenv.mkDerivation {
+  pname = "ponysay-modern-${shell-type}";
+  version = "unstable-2025-02-20";
+
+  src = ./..;
+
+  nativeBuildInputs = with pkgs; [
+    coreutils
+    gzip
+    makeWrapper
+    texinfo
+  ];
+
+  # ponysay's setup.py creates a self-extracting zip file
+  # which starts with a shebang. Replace those shebangs everywhere.
+  patchPhase = ''
+    substituteInPlace *.py src/*.py completion/*.py \
+      --replace '#!/usr/bin/env python3' '#!${python3}/bin/python3'
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    ${python3}/bin/python3 ./setup.py \
+      --prefix=$out \
+      --freedom=partial \
+      install \
+      --with-shared-cache=$out/share/ponysay \
+      --with-${shell-type}
+
+    runHook postInstall
+  '';
+
+  doCheck = true;
+}
---
+++ nix/package-zsh.nix
@@ -0,0 +1,3 @@
+(import ./package-core.nix) {
+  shell-type = "zsh";
+}
